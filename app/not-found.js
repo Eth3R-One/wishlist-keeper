611b4168fe1b184f45a4275942ca13464c0b8f74
Eth3R-One <refaythii@gmail.com>
2024-06-30 21:29:06 +0600

    update: ui

--- app/not-found.js
+++ app/not-found.js
@@ -1,10 +1,13 @@
-import { House } from "lucide-react";
+import { House, X } from "lucide-react";
+
 import Link from "next/link";
 
 export default function NotFound() {
 	return (
 		<div className="h-full w-full flex flex-col items-center justify-center p-20 ">
-			<h2 className="text-3xl mt-10 mb-2">Not Found</h2>
+			<h2 className="text-red-500 text-3xl mt-10 mb-2 flex flex-row items-center gap-2">
+				Not Found <X size={24} />
+			</h2>
 			<p>Could not find requested resource</p>
 			<Link
 				className="bg-slate-400 px-5 py-2 m-5 rounded-md text-slate-200 flex flex-row gap-2 mt-10 hover:bg-slate-950 hover:scale-110"
