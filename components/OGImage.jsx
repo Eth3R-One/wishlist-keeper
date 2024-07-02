d9675e517745e24d1a359f833c9ea8f11bee4354
Eth3R-One <refaythii@gmail.com>
2024-07-02 17:03:03 +0600

    update: og image ui

--- components/OGImage.jsx
+++ components/OGImage.jsx
@@ -1,7 +1,7 @@
 const OGImage = ({ title, body }) => {
 	return (
 		<div
-			tw="bg-indigo-950 rounded-xl h-full w-full flex flex-col items-center justify-center rounded-md"
+			tw="bg-indigo-950 rounded-xl h-full w-full flex flex-col items-center justify-center rounded-md text-slate-200 pb-10"
 			style={{
 				height: "100%",
 				width: "100%",
@@ -17,12 +17,12 @@ const OGImage = ({ title, body }) => {
 			<svg
 				width="75"
 				viewBox="0 0 75 65"
-				fill="#1e1b4b"
+				fill="#fff"
 				style={{ margin: "0 75px" }}
 			>
 				<path d="M37.59.25l36.95 64H.64l36.95-64z"></path>
 			</svg>
-			<div style={{ marginTop: 40 }} tw="text-indigo-800 text-5xl">
+			<div style={{ marginTop: 40 }} tw="text-5xl pb-10">
 				{title}
 			</div>
 			<div tw="text-l text-center px-5 mx-5">{body}</div>
