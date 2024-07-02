8f021e3e8305a62a3f8fb8ae9a1b76bf9c2232b0
Eth3R-One <refaythii@gmail.com>
2024-07-02 12:52:05 +0600

    add: links

--- links/navbarLinks.js
+++ links/navbarLinks.js
@@ -1,5 +1,9 @@
 export const navBarlinks = [
-	{ name: "My WishList", link: "/wishlists" },
+	{ name: "Home", link: "/" },
+	{ name: "About", link: "/about" },
+	{ name: "Features", link: "/features" },
+	{ name: "How It Works", link: "/how-it-works" },
+
 	{ name: "Login", link: "/login" },
 	{ name: "Sign up", link: "/sign-up" },
 ];
