bf05bd28e01e3c62ef1046f1b6a9880b23d16b73
Eth3R-One <refaythii@gmail.com>
2024-07-04 02:02:38 +0600

    fix: footer ui

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,15 +1,11 @@
-import { GridPattern } from "@/components/GridPattern";
 import { Github } from "lucide-react";
 import Link from "next/link";
 
 export function Footer() {
 	return (
-		<footer className="sticky top-[100vh] lg:py-10 sm:py-20 md:py-20 py-20 sm:mb-20 md:mb-20 mb-20">
-			<div className="absolute inset-x-0 top-0 h-16 text-slate-900/10 dark:text-slate-700 [mask-image:linear-gradient(white,transparent)]">
-				<GridPattern x="50%" />
-			</div>
-			<div className="relative text-center text-sm text-slate-400">
-				<div className="flex flex-row items-center justify-center gap-2 ">
+		<footer className="sticky top-[100vh] p-3 rounded-t-lg bg-indigo-200 dark:bg-indigo-900">
+			<div className="mx-auto">
+				<div className="flex justify-center">
 					<Link
 						href="https://github.com/eth3R-One/"
 						target="_blank"
