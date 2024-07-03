8f3f392b61408323912b95cd36f3b5b351cba73c
Eth3R-One <refaythii@gmail.com>
2024-07-03 16:40:08 +0600

    fix: ui

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,14 +4,14 @@ import Link from "next/link";
 
 export function Footer() {
 	return (
-		<footer className="sticky top-[100vh] pb-20 pt-5 sm:pb-32 sm:pt-14">
-			<div className="absolute inset-x-0 top-0 h-32 text-slate-900/10 [mask-image:linear-gradient(white,transparent)]">
+		<footer className="sticky top-[100vh] pb-20 pt-5 ">
+			<div className="absolute inset-x-0 top-0 h-16 text-slate-900/10 dark:text-slate-700 [mask-image:linear-gradient(white,transparent)]">
 				<GridPattern x="50%" />
 			</div>
 			<div className="relative text-center text-sm text-slate-400">
 				<span className="flex flex-row items-center justify-center gap-2 ">
 					<Link href="https://github.com/eth3R-One/" target="_blank">
-						<Github />
+						<Github className="dark:text-slate-200 text-indigo-900" />
 					</Link>
 				</span>
 			</div>
