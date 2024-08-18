4e71ea2d4e8d132a2f26679c1fcf64029c3ed5e7
Eth3R-One <refaythii@gmail.com>
2024-08-18 14:27:59 +0600

    add: auth actions

--- app/actions/authActions/index.js
+++ app/actions/authActions/index.js
@@ -10,6 +10,27 @@ export async function doSignOut() {
 }
 
 export async function doSignInWithGoogle() {
-	await dbConnect();
-	await signIn("google", { callbackUrl: `${process.env.BASE_URL}/wishlist` });
+	try {
+		await dbConnect();
+		await signIn("google", {
+			callbackUrl: `${process.env.BASE_URL}/wishlist`,
+		});
+	} catch (err) {
+		console.log(err);
+	}
+}
+
+export async function credntialLogin(formData) {
+	try {
+		await dbConnect();
+		const response = await signIn("credentials", {
+			email: formData.get("email"),
+			password: formData.get("password"),
+			redirect: false,
+		});
+		return response;
+	} catch (err) {
+		err;
+		// throw err;
+	}
 }
