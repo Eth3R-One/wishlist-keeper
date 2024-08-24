5bb77b6baca13ec976b31bd6784b47c9e351b464
Eth3R-One <refaythii@gmail.com>
2024-08-24 16:00:34 +0600

    removed: how works page

--- links/navbarLinks.js
+++ links/navbarLinks.js
@@ -2,7 +2,7 @@ export const navBarlinks = [
 	{ name: "Home", link: "/" },
 	{ name: "About", link: "/about" },
 	{ name: "Features", link: "/features" },
-	{ name: "How It Works", link: "/how-it-works" },
+	// { name: "How It Works", link: "/how-it-works" },
 
 	// { name: "Login", link: "/login" },
 	// { name: "Sign up", link: "/sign-up" },
