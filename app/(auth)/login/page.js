a375eca8bbaa2f95aa1d6b3b37b6f6dd70e41526
Eth3R-One <refaythii@gmail.com>
2024-08-30 14:19:13 +0600

    add: demo user

--- app/(auth)/login/page.js
+++ app/(auth)/login/page.js
@@ -2,6 +2,7 @@ import { auth } from "@/auth";
 import { LoginForm } from "./_components/login-form";
 import SocialLogins from "./_components/social-logins";
 import { redirect } from "next/navigation";
+import DemoUserInfo from "./_components/DemoUserInfo";
 
 const LoginPage = async () => {
 	const session = await auth();
@@ -11,6 +12,7 @@ const LoginPage = async () => {
 	return (
 		<>
 			<div className="w-full flex-col h-screen flex items-center justify-center">
+				<DemoUserInfo />
 				<div className="container">
 					<LoginForm />
 					<SocialLogins />
